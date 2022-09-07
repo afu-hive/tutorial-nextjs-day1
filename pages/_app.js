b979b7f9eae199e0e832fb466c5cc7ac19e15867
afu-hive <afu.sikarinp@hivecorps.com>
2022-09-07 14:09:05 +0700

    add style

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
+import { RecoilRoot } from 'recoil'
 import Header from '../src/components/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Header />
-      <div className="max-w-7xl mx-auto p-5 min-h-screen page">
-        <Component {...pageProps} />
+    <RecoilRoot>
+      <div>
+        <Header />
+        <div className="max-w-7xl mx-auto p-5 min-h-screen page">
+          <Component {...pageProps} />
+        </div>
+        <div className="bg-gray-400" style={{ height: 120 }}>
+          
+        </div>
       </div>
-      <div className="bg-gray-400" style={{ height: 120 }}>
-        
-      </div>
-    </div>
+    </RecoilRoot>
   )
 }
 
---
+++ src/stores/user.js
@@ -0,0 +1,10 @@
+import { atom } from 'recoil'
+
+const user = atom({
+  key: 'user',
+  default: {},
+})
+
+export default {
+  user,
+}
